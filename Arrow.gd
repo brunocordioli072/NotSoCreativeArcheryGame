5883fe96729fcea31bdcc61efc079a73f396a0e5
brunocordioli072 <brunocordioli072@gmail.com>
2022-10-19 14:40:20 -0300

    feat: implement auto restart after arrow throw

--- Arrow.gd
+++ Arrow.gd
@@ -1,16 +1,30 @@
 extends KinematicBody2D
 export(int) var SPEED: int = 0
-export(int) var VICTORY: int = 0
+export(int) var VICTORY: bool = false
 
 var velocity = Vector2()
 
+var count = 0
+var start_counting = false
+var initial_position = Vector2()
+
+func _ready():
+	initial_position = global_position
+
 func _physics_process(delta):
+	if count == 120:
+		global_position = initial_position
+		velocity = Vector2()
+
+	if start_counting == true and VICTORY != true:
+		count += 1
 
 	if Input.is_action_pressed("ui_right"):
+		start_counting = true
 		SPEED = 10
 		velocity = Vector2.RIGHT.normalized() * SPEED
 
-	var collision = move_and_collide(velocity)
 
+	var collision = move_and_collide(velocity)
 	if collision != null:
-		VICTORY = 1
+		VICTORY = true
--- Fundo.gd
+++ Fundo.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 func _process(delta):
 	var victory = get_node("Arrow").get("VICTORY")
-	if victory == 1:
+	if victory == true:
 		$Victory.visible = true
 	if Input.is_action_pressed("ui_left"):
 		get_tree().reload_current_scene()
